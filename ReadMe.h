8ebe3b14ba23881446d9492b4047269ca42455f0
肖峰 <xiaofeng@gaiay.cn>
2018-04-21 16:03:07 +0800

    修改ReadMe.h

--- ReadMe.h
+++ ReadMe.h
@@ -5,4 +5,5 @@ ZMCellEntity *cellEntity3 = self.vipInfoArray[1][0];
 ZMCellEntity *cellEntity4 = self.vipInfoArray[2][0];
 self.vipCategory.priceAlias = cellEntity4.value;
 
-// 哈哈哈1
+// 哈哈哈h1
+// 哈哈哈2
